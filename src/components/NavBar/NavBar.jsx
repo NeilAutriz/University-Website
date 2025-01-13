28d7aa3e5f848942d5486a2a1f5fb842d5dc6b25
NeilAutriz <mngautriz@gmail.com>
2025-01-13 16:59:19 +0800

    Added the header of the university

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,19 +2,19 @@ import '../NavBar/NavBar.css'
 import logo from '../../assets/logo.png'
 
 const NavBar = () => {
-  return (  
+  return (
     <nav className='nav-container'>
-        <img src={logo} className='nav-logo'/>
-        <ul className='nav-list'>
-            <li className='nav-list-item'>Home</li>
-            <li className='nav-list-item'>Program</li>
-            <li className='nav-list-item'>About</li>
-            <li className='nav-list-item'>Campus</li>
-            <li className='nav-list-item'>Testimonials</li>
-            <li className='nav-list-item'>
-                <button className='contact-button'>Contact Us</button>
-            </li>
-        </ul>
+      <img src={logo} className='nav-logo' />
+      <ul className='nav-list'>
+        <li className='nav-list-item'>Home</li>
+        <li className='nav-list-item'>Program</li>
+        <li className='nav-list-item'>About</li>
+        <li className='nav-list-item'>Campus</li>
+        <li className='nav-list-item'>Testimonials</li>
+        <li className='nav-list-item'>
+          <button className='contact-button'>Contact Us</button>
+        </li>
+      </ul>
     </nav>
   )
 }
