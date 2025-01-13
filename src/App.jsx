fc4f9e513acfe39740662bbfc80240e89c7624d5
NeilAutriz <mngautriz@gmail.com>
2025-01-13 14:02:07 +0800

    Created the navigation bar layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import './App.css'
+import NavBar from "./components/NavBar/Navbar"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
+      <NavBar />
     </>
   )
 }
