c2b5b6539903e3d4f5d18b64fe5d03e2f25362ae
NeilAutriz <mngautriz@gmail.com>
2025-01-14 14:40:14 +0800

    Added the logic for the scrolling effect

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,9 +1,18 @@
 import '../NavBar/NavBar.css'
 import logo from '../../assets/logo.png'
+import { useEffect, useState } from 'react'
 
 const NavBar = () => {
+  const [sticky, setSticky] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      window.scrollY > 400 ? setSticky(true) : setSticky(false)
+    })
+  }, [])
+
   return (
-    <nav className='nav-container'>
+    <nav className={`nav-container ${sticky == true ? 'dark-nav' : ''}`}>
       <img src={logo} className='nav-logo' />
       <ul className='nav-list'>
         <li className='nav-list-item'>Home</li>
